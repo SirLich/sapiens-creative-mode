6d18be2a6cd47021f1d4eee5ba12a261e087e778
SirLich <lrkoehler77@gmail.com>
2022-07-26 21:57:44 +0200

    Add preview

--- modInfo.lua
+++ modInfo.lua
@@ -3,7 +3,8 @@ local modInfo = {
     description = "Creative Mode in Sapiens! Instant Building, free skill unlocks, etc.",
     version = "0.0.1",
     type = "world",
-    developer = "SirLich"
+    developer = "SirLich",
+    preview = "assets/preview.jpg"
 }
 
 return modInfo
