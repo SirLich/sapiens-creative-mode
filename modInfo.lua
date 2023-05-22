03e9862a2138f8a34e1e656427f5e35122f83b55
SirLich <lrkoehler77@gmail.com>
2023-05-23 00:43:43 +0200

    Update for 1.2.0

--- modInfo.lua
+++ modInfo.lua
@@ -1,7 +1,7 @@
 local modInfo = {
     name = "Creative Mode",
     description = "Creative Mode in Sapiens! Instant Building, free skill unlocks, etc. \n\nThis mod requires you to also install the Hammerstone Modding Framework.",
-    version = "1.1.0",
+    version = "1.2.0",
     type = "world",
     developer = "SirLich",
     preview = "assets/preview.jpg"
